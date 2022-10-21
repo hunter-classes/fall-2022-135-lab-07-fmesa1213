7d4ce85d89ba3b6a7f291d4580b271dd0f1e2901
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-10-21 03:16:03 -0400

    Update unindent.cpp

--- unindent.cpp
+++ unindent.cpp
@@ -7,3 +7,27 @@ Assignment: Lab 7 A
 Here, I wrote a program that reads input from cin and prints out each input line with leading spaces removed.
 */
 
+#include <cctype>
+#include <iostream>
+#include <string>
+using namespace std;
+string removeLeadingSpaces(string line);
+
+int main()
+{
+string line;
+while(!cin.eof())
+{
+getline(cin, line);
+cout<<removeLeadingSpaces(line)<<endl;
+}
+return 0;
+}
+
+string removeLeadingSpaces(string line)
+{
+int ind = 0;
+while(isspace(line[ind]))
+ind++;
+return line.substr(ind);
+}
