0258159ed82915edce514a5ca4e05933d2f857fb
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-10-21 03:10:05 -0400

    Create bad-code.cpp

---
+++ bad-code.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Mike Zamansky
+Assignment: Lab 7 A (bad code)
+Here, I wrote a badly indented file for unident.
+*/
+
+int main(){
+    // Hi, I'm a program!
+    int x = 1; 
+    for(int i = 0; i < 10; i++) {
+        cout << i;
+        cout << endl;
+    } 
+  return 0;
+}
