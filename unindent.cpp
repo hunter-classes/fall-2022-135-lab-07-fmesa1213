1cfe75c8d4da5abcdbd544bf7756cc8e3c92306a
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-10-21 03:04:22 -0400

    Create unindent.cpp

---
+++ unindent.cpp
@@ -0,0 +1,9 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Mike Zamansky
+Assignment: Lab 7 A
+
+Here, I wrote a program that reads input from cin and prints out each input line with leading spaces removed.
+*/
+
