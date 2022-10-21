3b7b3f85215bdc0cca56ffed0cc913512c183685
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-10-21 03:21:20 -0400

    Update bad-code.cpp

--- bad-code.cpp
+++ bad-code.cpp
@@ -6,12 +6,20 @@ Assignment: Lab 7 A (bad code)
 Here, I wrote a badly indented file for unident.
 */
 
+#include <iostream>
+#include <fstream>
+using namespace std;
 int main(){
+    
+    fstream in;
+    in.open("unident.cpp");
     // Hi, I'm a program!
     int x = 1; 
     for(int i = 0; i < 10; i++) {
         cout << i;
         cout << endl;
     } 
+    in.close():
+    out.close();
   return 0;
 }
